0b80ad88277d7b86a07ff0d42b78163e3e773cb1
weiliya <viktoria.szabo.cv@gmail.com>
2017-07-06 11:37:27 +0200

    all lines are commented

--- comprehension.py
+++ comprehension.py
@@ -11,23 +11,23 @@ print('Well, ' + myName + ', I am thinking of a number between 1 and 20.') # pri
 while guessesTaken < 6:  # this while loop runs until the condition guessesTaken < 6 is true, it it is false, the loop ends
     print('Take a guess.')  # prints to console till guessesTaken < 6
     guess = input()  # assigns user input to variable guess
-    guess = int(guess)  # change variable type guess to integer
+    guess = int(guess)  # changes variable type guess to integer
 
     guessesTaken += 1   # adds 1 to variable guessesTaken each time the condition is true
 
-    if guess < number:  #
-        print('Your guess is too low.')
+    if guess < number:  # this if statement examines if the guess variable is smaller than the number variable, if this statement is true then
+        print('Your guess is too low.')  # prints 'Your guess is too low.' to console
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess > number:  # if this statement (variable guess is bigger than variable number) is true then
+        print('Your guess is too high.')  # prints 'Your guess is too high.' to console
 
-    if guess == number:
-        break
+    if guess == number:  # if statement 'variable guess equals to variable number' is true then
+        break  # the break statement terminates the while loop and jumps to the next line of code after this block
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+if guess == number:  # if statement 'variable guess equals to variable number' is true then
+    guessesTaken = str(guessesTaken)  # changes variable type guessesTaken to string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # outputs the text in between the brackets and the user input assigned to variable myName
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess != number:  # if statement 'guess is not equal to number' is true, then
+    number = str(number)  # changes variable type number to string
+    print('Nope. The number I was thinking of was ' + number)  # prints the text and the string value of number variable to the console
